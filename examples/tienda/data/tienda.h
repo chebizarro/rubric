0308677277fc46b2b58f32a5643b8eb1128e4aaf
chebizarro@gmail.com <chebizarro@gmail.com>
2017-03-16 08:45:43 -0700

    First commit

--- examples/tienda/data/tienda.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_rubric_gtk--target=resources.h_H__
-#define __RESOURCE_rubric_gtk--target=resources.h_H__
-
-#include <gio/gio.h>
-
-extern GResource *rubric_gtk--target=resources.h_get_resource (void);
-#endif
